ea61dd4dc98c564766e8c1a25ab87259150c0991
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-06 11:33:19 -0600

    Add additional mocks/stubs to season test file
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: Patrick-Duvall <duvallp208@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- test/season_test.rb
+++ test/season_test.rb
@@ -1,24 +1,27 @@
 require 'csv'
 require 'mocha/minitest'
+require './lib/season'
 
 class SeasonTest < MiniTest::Test
 
   def setup
-    @season = mock
     @game_1 = mock
+    @game_1.stubs(type: "R")
     @game_2 = mock
+    @game_2.stubs(type: "R")
     @game_3 = mock
+    @game_3.stubs(type: "R")
     @game_4 = mock
+    @game_4.stubs(type: "P")
+    @season = Season.new([@game_1, @game_2, @game_3, @game_4])
   end
 
   def test_has_regular_and_post_season_games
-    @season.stubs(regular_season_games: [@game_1, @game_2, @game_3], post_season_games: [@game_4])
     assert_equal [@game_1, @game_2, @game_3], @season.regular_season_games
     assert_equal [@game_4], @season.post_season_games
   end
 
   def test_returns_win_percentage
-    @season.stubs(win_percentage: 0.5)
     assert_equal 0.5, @season.win_percentage
   end
 
