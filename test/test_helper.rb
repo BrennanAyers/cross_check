5e49859e48cdd75d60b73242b96e215580f9b246
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-06 16:06:50 -0600

    Add lib files to test helper
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: Patrick-Duvall <duvallp208@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,14 @@
 require 'simplecov'
 require 'simplecov-console'
-require "minitest/autorun"
-require "csv"
-require "./lib/game"
-require "./lib/stat_tracker.rb"
+require 'minitest/autorun'
+require 'csv'
+require './lib/game_team'
+require './lib/game'
+require './lib/team'
+require './lib/stat_tracker.rb'
+require './lib/season'
+require './lib/team_specific_stats'
+
 SimpleCov.formatter = SimpleCov::Formatter::Console
 
 SimpleCov.start
